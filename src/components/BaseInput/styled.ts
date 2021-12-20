f527ee88c4ed776c75455f969aea8235635d4a81
makotty <hal.m.90215@gmail.com>
2021-12-20 13:48:32 +0900

    ベースとなるInputのコンポーネントのスタイリング（幅を100%に）

---
+++ src/components/BaseInput/styled.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+import { TextField } from '@mui/material'
+
+const FullWidthTextField = styled(TextField)`
+  width: 100%;
+`
+
+export default FullWidthTextField
