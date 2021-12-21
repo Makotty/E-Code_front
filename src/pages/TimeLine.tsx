8a886f8ccb20e8070edbcb276e2440e22acb126b
makotty <hal.m.90215@gmail.com>
2021-12-21 18:30:28 +0900

    ログアウト(Reader)の処理を追加

--- src/pages/TimeLine.tsx
+++ src/pages/TimeLine.tsx
@@ -1,11 +1,41 @@
+// React
+import { useState } from 'react'
 import type { VFC } from 'react'
 
+import { useNavigate } from 'react-router-dom'
+
+// MUI
+import { Button } from '@mui/material'
+
+// Firebase
+import { AuthError, signOut } from 'firebase/auth'
+import auth from '../firebase'
+
+// Components
 import BaseLayout from '../components/BaseLayout'
 
 const TimeLine: VFC = () => {
+  const navigate = useNavigate()
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const handleReaderLogout = async () => {
+    await signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate('/')
+      })
+      .catch((error: AuthError) => {
+        // An error happened.
+        setErrorMessage(error.message)
+      })
+  }
   return (
     <BaseLayout>
       <h2>タイムライン</h2>
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      <Button variant="contained" onClick={handleReaderLogout}>
+        ログアウト(Reader)
+      </Button>
     </BaseLayout>
   )
 }
