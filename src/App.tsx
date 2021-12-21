7a3e94b0204f0327453b2d56bcc52e2de9736789
makotty <hal.m.90215@gmail.com>
2021-12-21 12:09:02 +0900

    作成したContextでWrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import type { VFC } from 'react'
 import SignUp from './pages/SignUp'
 
+import { OAuthContextProvider } from './contexts/OAuthContext'
+
 const App: VFC = () => {
-  return <SignUp />
+  return (
+    <OAuthContextProvider>
+      <SignUp />
+    </OAuthContextProvider>
+  )
 }
 
 export default App
