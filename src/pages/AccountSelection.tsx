90b04285dfd3b58816e13cd39d4581452428efa3
makotty <hal.m.90215@gmail.com>
2021-12-21 17:56:20 +0900

    コード修正

--- src/pages/AccountSelection.tsx
+++ src/pages/AccountSelection.tsx
@@ -1,21 +1,19 @@
 import type { VFC } from 'react'
 import { Link } from 'react-router-dom'
 
-import { Button, Paper } from '@mui/material'
+import { Button } from '@mui/material'
 
 import BaseLayout from '../components/BaseLayout'
 
 const AccountSelection: VFC = () => {
   return (
     <BaseLayout>
-      <Paper>
-        <Button variant="contained" component={Link} to="/reader_signup">
-          読む人で登録する。
-        </Button>
-        <Button variant="contained" component={Link} to="/corder_signup">
-          書く人で登録する。
-        </Button>
-      </Paper>
+      <Button variant="contained" component={Link} to="/reader_signup">
+        読む人で登録する。
+      </Button>
+      <Button variant="contained" component={Link} to="/corder_signup">
+        書く人で登録する。
+      </Button>
     </BaseLayout>
   )
 }
