d543afdecc691962bf5f5085a5e69c1b1ed4b139
makotty <hal.m.90215@gmail.com>
2021-12-22 23:43:49 +0900

    不要になったコードの削除

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -5,8 +5,6 @@ import { getCurrentUser } from '@lib/api/auth'
 
 export const AuthContext = createContext(
   {} as {
-    loading: boolean
-    setLoading: Dispatch<SetStateAction<boolean>>
     isSignedIn: boolean
     setIsSignedIn: Dispatch<SetStateAction<boolean>>
     corderCurrentUser: CorderUser | undefined
@@ -25,7 +23,6 @@ type AuthContextProviderProps = {
 export const AuthContextProvider: VFC<AuthContextProviderProps> = ({
   children
 }) => {
-  const [loading, setLoading] = useState<boolean>(true)
   const [isSignedIn, setIsSignedIn] = useState<boolean>(false)
   const [corderCurrentUser, setCorderCurrentUser] = useState<
     CorderUser | undefined
@@ -36,46 +33,33 @@ export const AuthContextProvider: VFC<AuthContextProviderProps> = ({
     try {
       const response = await getCurrentUser()
 
-      console.log(response)
-
       if (response?.data.isLogin === true) {
         setIsSignedIn(true)
         setCorderCurrentUser(response?.data.data)
       } else {
-        throw new Error(response?.data.message)
+        // throw new Error(response?.data.message)
       }
     } catch (error) {
-      console.log(error)
+      // console.log(error)
     }
-
-    setLoading(false)
   }
 
   const value = useMemo(() => {
     return {
-      loading,
-      setLoading,
       isSignedIn,
       setIsSignedIn,
       corderCurrentUser,
       setCorderCurrentUser
     }
-  }, [
-    loading,
-    setLoading,
-    isSignedIn,
-    setIsSignedIn,
-    corderCurrentUser,
-    setCorderCurrentUser
-  ])
+  }, [isSignedIn, setIsSignedIn, corderCurrentUser, setCorderCurrentUser])
 
   useEffect(() => {
     handleGetCurrentUser()
-      .then((result) => {
-        console.log(result)
+      .then(() => {
+        //
       })
-      .catch((error) => {
-        console.log(error)
+      .catch(() => {
+        //
       })
   }, [setCorderCurrentUser])
 
