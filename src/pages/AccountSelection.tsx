f50aed987222153e5a5e430834f65aca0cd7ab31
makotty <hal.m.90215@gmail.com>
2021-12-22 23:46:48 +0900

    どちらかでログインしていればタイムライン画面へ遷移

--- src/pages/AccountSelection.tsx
+++ src/pages/AccountSelection.tsx
@@ -1,11 +1,20 @@
 import type { VFC } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
 
 import { Button } from '@mui/material'
 
 import BaseLayout from '@components/BaseLayout'
+import { useOAuthContext } from '@contexts/OAuthContext'
+import { useAuthContext } from '@contexts/AuthContext'
 
 const AccountSelection: VFC = () => {
+  const { corderCurrentUser } = useAuthContext()
+  const { oAuthCurrentUser } = useOAuthContext()
+
+  if (corderCurrentUser || oAuthCurrentUser) {
+    return <Navigate to="/timeline" />
+  }
+
   return (
     <BaseLayout>
       <Button variant="contained" component={Link} to="/reader_signup">
