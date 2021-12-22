9a68f92f09b9f77b74152087f2eb3ee2542a012b
makotty <hal.m.90215@gmail.com>
2021-12-22 23:49:00 +0900

    ログインしているユーザーの種類に応じて画面の表示を変更

--- src/pages/TimeLine.tsx
+++ src/pages/TimeLine.tsx
@@ -1,9 +1,9 @@
 // React
-import { memo, useState } from 'react'
+import { useState } from 'react'
 import type { VFC } from 'react'
 
 // React Router
-import { Link, Navigate, useNavigate } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 // MUI
 import { Button } from '@mui/material'
@@ -27,8 +27,7 @@ import { corderLogOut } from '@lib/api/auth'
 import auth from '../firebase'
 
 const TimeLine: VFC = () => {
-  const { loading, isSignedIn, setIsSignedIn, corderCurrentUser } =
-    useAuthContext()
+  const { isSignedIn, setIsSignedIn, corderCurrentUser } = useAuthContext()
   const { oAuthCurrentUser } = useOAuthContext()
   const navigate = useNavigate()
   const [errorMessage, setErrorMessage] = useState('')
@@ -45,16 +44,11 @@ const TimeLine: VFC = () => {
       })
   }
 
-  // oAuthCurrentUserを保持していなければ"/"に遷移
-  if (!oAuthCurrentUser) {
-    return <Navigate to="/" />
-  }
-
   const handleCorderSignOut = async () => {
     try {
-      const res = await corderLogOut()
+      const response = await corderLogOut()
 
-      if (res.data.success === true) {
+      if (response.data.success === true) {
         // ログアウト出来たらCookieを削除
         Cookies.remove('_access_token')
         Cookies.remove('_client')
@@ -62,55 +56,38 @@ const TimeLine: VFC = () => {
 
         setIsSignedIn(false)
 
-        navigate('/corder_login')
-
-        console.log('ログアウトできました')
+        window.location.reload()
       } else {
         console.log('ログアウトに失敗しました')
       }
     } catch (error) {
       console.log(error)
     }
+
+    navigate('/')
   }
 
-  const CorderAuthButtons = memo(() => {
-    // 認証完了後はサインアウト用のボタンを表示
-    // 未認証時は認証用のボタンを表示
-    if (!loading) {
-      if (isSignedIn) {
-        return <Button onClick={handleCorderSignOut}>Log Out</Button>
-      }
-      return (
-        <>
-          <Button component={Link} to="/corder_login">
-            Log In
-          </Button>
-          <Button component={Link} to="/corder_signup">
-            Sign Up
-          </Button>
-        </>
-      )
-    }
-    return null
-  })
+  if (!corderCurrentUser && !oAuthCurrentUser) {
+    return <Navigate to="/" />
+  }
 
   return (
     <BaseLayout>
       <h2>タイムライン</h2>
       {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-      <Button variant="contained" onClick={handleReaderLogout}>
-        ログアウト(Reader)
-      </Button>
+      {oAuthCurrentUser && (
+        <Button variant="contained" onClick={handleReaderLogout}>
+          ログアウト(Reader)
+        </Button>
+      )}
 
-      {isSignedIn && corderCurrentUser ? (
+      {isSignedIn && corderCurrentUser && (
         <>
           <h2>Email: {corderCurrentUser?.email}</h2>
           <h2>Name: {corderCurrentUser?.name}</h2>
+          <Button onClick={handleCorderSignOut}>Log Out</Button>
         </>
-      ) : (
-        <h2>Not signed in</h2>
       )}
-      <CorderAuthButtons />
     </BaseLayout>
   )
 }
