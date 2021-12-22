9b415276079b16858a8ef4610315c1d06ecff65c
makotty <hal.m.90215@gmail.com>
2021-12-23 03:06:17 +0900

    BaseModalのスタイリング

---
+++ src/components/BaseModal/styled.ts
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled'
+
+export const OverlayModal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const ModalCard = styled.div`
+  background-color: white;
+  width: 50%;
+  border-radius: 4px;
+`
