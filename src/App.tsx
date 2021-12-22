3ba5db95df35aaddd4cdeb1fd5b83893472a5b52
makotty <hal.m.90215@gmail.com>
2021-12-22 22:07:07 +0900

    Corder用のサインアップ、ログイン画面のルーティング

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import { OAuthContextProvider } from '@contexts/OAuthContext'
 
 // Pages
 import AccountSelection from '@pages/AccountSelection'
+import CorderLogin from '@pages/CorderLogIn'
+import CorderSignUp from '@pages/CorderSignUp'
 import ReaderLogin from '@pages/ReaderLogin'
 import ReaderSignUp from '@pages/ReaderSignUp'
 import TimeLine from '@pages/TimeLine'
@@ -27,6 +29,8 @@ const App: VFC = () => {
               <Route path="/" element={<AccountSelection />} />
               <Route path="/reader_signup" element={<ReaderSignUp />} />
               <Route path="/reader_login" element={<ReaderLogin />} />
+              <Route path="/corder_signup" element={<CorderSignUp />} />
+              <Route path="/corder_login" element={<CorderLogin />} />
               <Route path="/timeline" element={<TimeLine />} />
             </Routes>
           </BrowserRouter>
