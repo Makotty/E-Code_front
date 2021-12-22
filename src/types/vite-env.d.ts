feb8a26a074599459ecf364da75aa09bd1e85d92
makotty <hal.m.90215@gmail.com>
2021-12-23 03:02:27 +0900

    S3用の環境変数の型定義

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -7,6 +7,11 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_STORAGE_BUCKET: string
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string
   readonly VITE_FIREBASE_APP_ID: string
+
+  readonly VITE_S3_BUCKET: string
+  readonly VITE_S3_REGION: string
+  readonly VITE_S3_ACCESS_KEY: string
+  readonly VITE_S3_SECRET_ACCESS_KEY: string
 }
 
 interface ImportMeta {
