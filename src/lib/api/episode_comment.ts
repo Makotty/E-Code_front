2472b1ae3fc53bb0c5de84d75b013df554472971
makotty <hal.m.90215@gmail.com>
2021-12-25 08:24:28 +0900

    コメントを投稿するためのクライアントの作成

---
+++ src/lib/api/episode_comment.ts
@@ -0,0 +1,17 @@
+// Cookies
+import Cookies from 'js-cookie'
+
+import client from './client'
+
+// エピソードコメント新規作成
+const createEpisodeComment = (params: { content: string; episodeId: number }) => {
+  return client.post('/episode_comments', params, {
+    headers: {
+      'access-token': Cookies.get('_access_token') ?? '',
+      client: Cookies.get('_client') ?? '',
+      uid: Cookies.get('_uid') ?? ''
+    }
+  })
+}
+
+export default createEpisodeComment
