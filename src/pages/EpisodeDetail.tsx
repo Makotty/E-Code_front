6895988f76f62b14831f4d33bd530c40b073d2b1
makotty <hal.m.90215@gmail.com>
2021-12-25 08:26:29 +0900

    コメント機能の実装(エピソード詳細画面)

--- src/pages/EpisodeDetail.tsx
+++ src/pages/EpisodeDetail.tsx
@@ -1,13 +1,13 @@
 // React
-import { useEffect, useState } from 'react'
-import type { VFC } from 'react'
+import { useState, useEffect } from 'react'
+import type { VFC, MouseEvent, ChangeEvent } from 'react'
 
 // React Router
 import { useNavigate, useParams } from 'react-router-dom'
 import type { Params } from 'react-router-dom'
 
 // Mui
-import { Button } from '@mui/material'
+import { Avatar, Button, Paper } from '@mui/material'
 
 // Components
 import BaseLayout from '@components/BaseLayout'
@@ -16,11 +16,16 @@ import BaseLayout from '@components/BaseLayout'
 import { getEpisodeDetail } from '@lib/api/episode'
 
 // Types
+import createEpisodeComment from '@lib/api/episode_comment'
+import EpisodeTextArea from '@components/EpisodeTextArea'
 import { EpisodeData } from '../types/EpisodeData'
 
 const EpisodeDetail: VFC = () => {
   const navigate = useNavigate()
   const [errorMessage, setErrorMessage] = useState('')
+
+  const [episodeCommentValue, setEpisodeCommentValue] = useState('')
+  const [episodeId, setEpisodeId] = useState<number>(0)
 
   const [episodeData, setEpisodeData] = useState<EpisodeData | undefined>()
   const query = useParams()
@@ -29,15 +34,35 @@ const EpisodeDetail: VFC = () => {
     const { id } = data
 
     if (id) {
-      try {
-        const respone = await getEpisodeDetail(id)
-        setEpisodeData(respone.data)
-      } catch (error) {
+      await getEpisodeDetail(id)
+        .then((response) => {
+          setEpisodeData(response.data)
+          setEpisodeId(Number(id))
+        })
+        .catch((error) => {
+          if (error) {
+            setErrorMessage('何らかのエラーが発生しました')
+          }
+        })
+    }
+  }
+
+  const handleChangeCreateArea = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setEpisodeCommentValue(event.currentTarget.value)
+  }
+
+  const handleSubmit = async (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+
+    await createEpisodeComment({ content: episodeCommentValue, episodeId })
+      .then(() => {
+        window.location.reload()
+      })
+      .catch((error) => {
         if (error) {
           setErrorMessage('何らかのエラーが発生しました')
         }
-      }
-    }
+      })
   }
 
   useEffect(() => {
@@ -45,23 +70,51 @@ const EpisodeDetail: VFC = () => {
       .then(() => {
         //
       })
-      .catch(() => {
-        //
+      .catch((error) => {
+        if (error) {
+          setErrorMessage('何らかのエラーが発生しました')
+        }
       })
   }, [query])
 
   const handleBack = () => {
-    navigate('/episode_list')
+    navigate(-1)
+  }
+
+  const replaceNewLine = episodeData?.content.replaceAll(/\n/g, '<br>')
+  const episodeContent = document.getElementById('episodeContent')
+  if (episodeContent && replaceNewLine) {
+    episodeContent.innerHTML = replaceNewLine
   }
 
   return (
     <BaseLayout>
-      <h2>Episode Detail</h2>
-      {errorMessage && <p>{errorMessage}</p>}
-      <div>ID:{episodeData?.id}</div>
-      <div>Content:{episodeData?.content}</div>
+      <Paper>
+        <h2>Episode Detail</h2>
+        {errorMessage && <p>{errorMessage}</p>}
+        <Avatar
+          src={episodeData?.contributorImage}
+          alt="アカウントアイコン"
+          sx={{ width: 64, height: 64 }}
+        />
+        <p>{episodeData?.contributorName}</p>
+        <div id="episodeContent" />
 
-      <Button onClick={handleBack}>戻る</Button>
+        <form>
+          <EpisodeTextArea onChange={handleChangeCreateArea} />
+        </form>
+        <Button type="submit" variant="contained" onClick={handleSubmit}>
+          投稿する
+        </Button>
+
+        {episodeData?.episodeComments &&
+          episodeData?.episodeComments.map((data) => {
+            const { id, content } = data
+            return <p key={id}>{content}</p>
+          })}
+
+        <Button onClick={handleBack}>戻る</Button>
+      </Paper>
     </BaseLayout>
   )
 }
