cc7e73fe7052ca722ca7c869046b1b392d7a0ea2
makotty <hal.m.90215@gmail.com>
2021-12-25 08:23:43 +0900

    コメント機能の実装

--- src/containers/EpisodeListCard.tsx
+++ src/containers/EpisodeListCard.tsx
@@ -11,7 +11,7 @@ import { Accordion, AccordionDetails, AccordionSummary, Avatar, Button } from '@
 import { CorderUser } from '@interfaces/index'
 
 // Types
-import { EpisodeData } from '../types/EpisodeData'
+import { EpisodeCommentsData, EpisodeData } from '../types/EpisodeData'
 
 type EpisodeListCardProps = {
   episodeDataList: EpisodeData[] | undefined
@@ -25,7 +25,8 @@ const EpisodeListCard: VFC<EpisodeListCardProps> = (props) => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column-reverse' }}>
       {episodeDataList?.map((contents: EpisodeData) => {
-        const { id, content, contributorName, contributorImage, userId } = contents
+        const { id, content, contributorName, contributorImage, userId, episodeComments } = contents
+
         return (
           <Accordion key={id}>
             <AccordionSummary>
@@ -41,6 +42,12 @@ const EpisodeListCard: VFC<EpisodeListCardProps> = (props) => {
               <Link to={`/episode_list/${id}`}>
                 <p>{content}</p>
               </Link>
+
+              {episodeComments &&
+                episodeComments.slice(-5).map((data: EpisodeCommentsData) => {
+                  return <p key={data.id}>{data.content}</p>
+                })}
+
               {corderCurrentUser?.id === userId ? (
                 <Button component={Link} to={`/episode_edit/${id}`}>
                   更新
