2dd105e7d5a74fca8bb519c5928cb3d7f92410f0
makotty <hal.m.90215@gmail.com>
2021-12-25 02:05:42 +0900

    ログインしているユーザーの投稿を表示するためのクライアントの作成

---
+++ src/lib/api/user_episode.ts
@@ -0,0 +1,19 @@
+import { AxiosResponse } from 'axios'
+import Cookies from 'js-cookie'
+import { EpisodeData } from 'src/types/EpisodeData'
+
+import client from './client'
+
+type getEpisodeListData = EpisodeData[]
+
+const getUserEpisodes = (id: number): Promise<AxiosResponse<getEpisodeListData>> => {
+  return client.get(`/user_episodes/${id}`, {
+    headers: {
+      'access-token': Cookies.get('_access_token') ?? '',
+      client: Cookies.get('_client') ?? '',
+      uid: Cookies.get('_uid') ?? ''
+    }
+  })
+}
+
+export default getUserEpisodes
