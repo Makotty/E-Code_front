5e82a24c3fc6c76bc6f4c9c519eca0ac4a2779e5
makotty <hal.m.90215@gmail.com>
2021-12-25 23:28:42 +0900

    アカウント選択画面の実装と、スタイリング

--- src/pages/AccountSelection.tsx
+++ src/pages/AccountSelection.tsx
@@ -1,34 +1,53 @@
 import type { VFC } from 'react'
 import { Link, Navigate } from 'react-router-dom'
 
-import { Button } from '@mui/material'
+import { Button, Container, Paper, Stack } from '@mui/material'
 
-import BaseLayout from '@components/BaseLayout'
 import { useOAuthContext } from '@contexts/OAuthContext'
 import { useAuthContext } from '@contexts/AuthContext'
+import { AutoStories, HistoryEdu } from '@mui/icons-material'
+import { ECodeIconBox, AccountSelectionHead } from '@styles/pages/AccountSelectionStyled'
 
 const AccountSelection: VFC = () => {
   const { corderCurrentUser } = useAuthContext()
   const { readerCurrentUser } = useOAuthContext()
 
-  if (corderCurrentUser || readerCurrentUser) {
+  if (readerCurrentUser || corderCurrentUser) {
     return <Navigate to="/timeline" />
   }
 
   return (
-    <BaseLayout>
-      <Button variant="contained" component={Link} to="/reader_signup">
-        読む人
-      </Button>
-      <Button
-        variant="contained"
-        color="secondary"
-        component={Link}
-        to="/corder_signup"
-      >
-        書く人
-      </Button>
-    </BaseLayout>
+    <Container maxWidth="sm">
+      <Stack spacing={4}>
+        <Paper sx={{ mt: '64px' }} elevation={2}>
+          <ECodeIconBox>
+            <img src="../../src/assets/images/e-code-icon.svg" alt="E-Codeのアイコン" />
+          </ECodeIconBox>
+          <AccountSelectionHead>E-Codeへようこそ(⌒∇⌒)</AccountSelectionHead>
+        </Paper>
+        <Stack direction="row" spacing={4}>
+          <Button
+            variant="contained"
+            component={Link}
+            to="/reader_signup"
+            startIcon={<AutoStories />}
+            fullWidth
+          >
+            E-READER
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            component={Link}
+            to="/corder_signup"
+            startIcon={<HistoryEdu />}
+            fullWidth
+          >
+            E-CORDER
+          </Button>
+        </Stack>
+      </Stack>
+    </Container>
   )
 }
 
