4d2a427ed8143faa0968fe089cae6e2573b37914
makotty <hal.m.90215@gmail.com>
2021-12-25 23:22:19 +0900

    ユーザーがログインしているかに応じでのAppBarの切り替えの処理の追加

---
+++ src/containers/Layout.tsx
@@ -0,0 +1,26 @@
+import type { ReactNode, VFC } from 'react'
+
+import { Container } from '@mui/material'
+import LogedInHeader from '@containers/LogedInHeader'
+import { useAuthContext } from '@contexts/AuthContext'
+import { useOAuthContext } from '@contexts/OAuthContext'
+import BaseHeader from '../components/BaseHeader'
+import BaseFooter from '../components/BaseFooter'
+
+type LayoutProps = {
+  children: ReactNode
+}
+
+const Layout: VFC<LayoutProps> = ({ children }) => {
+  const { corderCurrentUser } = useAuthContext()
+  const { readerCurrentUser } = useOAuthContext()
+  return (
+    <>
+      {corderCurrentUser || readerCurrentUser ? <LogedInHeader /> : <BaseHeader />}
+      <Container maxWidth="sm">{children}</Container>
+      <BaseFooter />
+    </>
+  )
+}
+
+export default Layout
