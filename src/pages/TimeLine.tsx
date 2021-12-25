86b1e599865aa82c6d5767b3971176e0b644be99
makotty <hal.m.90215@gmail.com>
2021-12-25 23:17:06 +0900

    ログアウトの処理をヘッダーで行うため、不要になったコードの削除

--- src/pages/TimeLine.tsx
+++ src/pages/TimeLine.tsx
@@ -5,72 +5,29 @@ import type { VFC } from 'react'
 // React Router
 import { Navigate, useNavigate } from 'react-router-dom'
 
-// MUI
-import { Avatar, Button } from '@mui/material'
-
-// Js-Cookies
-import Cookies from 'js-cookie'
-
-// Firebase
-import { AuthError, signOut } from 'firebase/auth'
-
 // Contexts
 import { useAuthContext } from '@contexts/AuthContext'
 import { useOAuthContext } from '@contexts/OAuthContext'
 
 // Components
-import BaseLayout from '@components/BaseLayout'
+import Layout from '@containers/Layout'
 
 // Containers
 import EpisodeListCard from '@containers/EpisodeListCard'
 
 // Lib
-import { corderLogOut } from '@lib/api/auth'
-import auth from '@lib/firebase'
+
 import { deleteEpisode, getEpisodeList } from '@lib/api/episode'
 
 // Types
+import EpisodeCreateButton from '@containers/EpisodeCreateButton'
 import { EpisodeData } from '../types/EpisodeData'
 
 const TimeLine: VFC = () => {
-  const navigate = useNavigate()
   const [errorMessage, setErrorMessage] = useState('')
 
-  const { isSignedIn, setIsSignedIn, corderCurrentUser } = useAuthContext()
+  const { corderCurrentUser } = useAuthContext()
   const { readerCurrentUser } = useOAuthContext()
-
-  const handleReaderLogout = async () => {
-    await signOut(auth)
-      .then(() => {
-        // Sign-out successful.
-        navigate('/')
-      })
-      .catch((error: AuthError) => {
-        // An error happened.
-        setErrorMessage(error.message)
-      })
-  }
-
-  const handleCorderSignOut = async () => {
-    await corderLogOut()
-      .then((response) => {
-        if (response.data.success === true) {
-          // ログアウト出来たらCookieを削除
-          Cookies.remove('_access_token')
-          Cookies.remove('_client')
-          Cookies.remove('_uid')
-          setIsSignedIn(false)
-          window.location.reload()
-        }
-      })
-      .catch((error) => {
-        if (error) {
-          setErrorMessage('何らかのエラーが発生しました')
-        }
-      })
-
-    navigate('/')
-  }
 
   const [episodeDataList, setEpisodeDataList] = useState<EpisodeData[] | undefined>([])
 
@@ -121,28 +78,12 @@ const TimeLine: VFC = () => {
   }
 
   return (
-    <BaseLayout>
+    <Layout>
       <h2>タイムライン</h2>
-      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-      {readerCurrentUser && (
-        <Button variant="contained" onClick={handleReaderLogout}>
-          ログアウト(Reader)
-        </Button>
-      )}
 
-      {isSignedIn && corderCurrentUser && (
-        <>
-          <h2>Email: {corderCurrentUser?.email}</h2>
-          <h2>Name: {corderCurrentUser?.name}</h2>
-          <Avatar
-            src={corderCurrentUser?.fileUrl}
-            alt="アカウントアイコン"
-            sx={{ width: 64, height: 64 }}
-          />
-          <h2>Name: {corderCurrentUser?.birthDay}</h2>
-          <Button onClick={handleCorderSignOut}>ログアウト(Corder)</Button>
-        </>
-      )}
+      <EpisodeCreateButton />
+
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
 
       <EpisodeListCard
         episodeDataList={episodeDataList}
@@ -150,7 +91,7 @@ const TimeLine: VFC = () => {
         corderCurrentUser={corderCurrentUser}
         sliceStartNumber={-50}
       />
-    </BaseLayout>
+    </Layout>
   )
 }
 
