52e8e992d8c74b8a4376d1886cc61bc192dda14c
makotty <hal.m.90215@gmail.com>
2021-12-27 14:20:33 +0900

    EpisodeEditStyledのスタイリング

---
+++ src/assets/styles/pages/EpisodeEditStyled.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+import { Paper } from '@mui/material'
+
+export const EpisodeEditPaper = styled(Paper)`
+  display: flex;
+  flex-direction: column;
+  padding: 32px;
+`
+
+export const EpisodeEditPaperstyled = styled(Paper)`
+  display: flex;
+`
