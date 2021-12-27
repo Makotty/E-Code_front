1c6b88638b511a249a58b92b4d8129b589951cda
makotty <hal.m.90215@gmail.com>
2021-12-27 11:46:58 +0900

    Readerのログイン、サインアップ画面のスタイリング

---
+++ src/assets/styles/pages/ReaderSign.ts
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled'
+import { Box } from '@mui/material'
+
+export const ECodeIcon = styled(Box)`
+  width: 96px;
+`
+
+export const ECodeIconBox = styled(Box)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 64px 0;
+`
+
+export const AvatorBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 24px;
+`
