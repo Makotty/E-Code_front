3c51d7eff1f8c46893ae7de7e22c439b4b0d11ac
makotty <hal.m.90215@gmail.com>
2021-12-27 14:20:50 +0900

    TimeLineStyledのスタイリング

---
+++ src/assets/styles/pages/TimeLineStyled.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+import { Box } from '@mui/material'
+
+const EpisodeCreateBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+`
+
+export default EpisodeCreateBox
