589bed8fb2673cf273d7441e6ac085f034156d61
makotty <hal.m.90215@gmail.com>
2021-12-27 11:46:23 +0900

    エピソード作成ボタンのスタイリング

---
+++ src/assets/styles/EpisodeCreateButtonStyled.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+import { Fab } from '@mui/material'
+
+const EpiCreateButton = styled(Fab)`
+  position: fixed;
+  right: 32px;
+  bottom: 32px;
+  z-index: 999;
+`
+
+export default EpiCreateButton
