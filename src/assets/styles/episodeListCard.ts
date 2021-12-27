438e2e56cc694f4f353ce8a23ae57e4d4a01d6c9
makotty <hal.m.90215@gmail.com>
2021-12-27 14:19:59 +0900

    episodeListCardのスタイリング

---
+++ src/assets/styles/episodeListCard.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled'
+
+export const ContributorInfo = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+export const ContributorInfoName = styled.h3`
+  margin-left: 32px;
+`
+
+export const CommentContributorInfoName = styled.h4`
+  margin: 8px 0 8px 32px;
+`
+export const CommentContent = styled.p`
+  font-size: 14px;
+`
+export const CommentContentDate = styled.p`
+  font-size: 12px;
+`
